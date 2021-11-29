3b8801fbfac432f6b156e172783ef127714b35ae
Eric Olszewski <eolszewski@gmail.com>
2021-11-29 09:17:11 -0600

    Fix .env vars in hardhat config.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
-require("@nomiclabs/hardhat-waffle");
 require('dotenv').config()
+require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,9 +21,20 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       chainId: 42,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+      accounts: [
+        `${process.env.DEPLOYER_PRIVATE_KEY}`, 
+        `${process.env.CLIENT_PRIVATE_KEY}`
+      ],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      chainId: 5,
+      accounts: [
+        `${process.env.DEPLOYER_PRIVATE_KEY}`, 
+        `${process.env.CLIENT_PRIVATE_KEY}`
+      ],
     }
   }
 };
