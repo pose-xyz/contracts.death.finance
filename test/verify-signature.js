789033a348c51610f1434b9a929bab2d584b10a1
Eric Olszewski <eolszewski@gmail.com>
2022-02-17 11:31:59 -0600

    Remove betting from FightClub.

--- test/verify-signature.js
+++ test/verify-signature.js
@@ -1,34 +1,34 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// describe("VerifySignature", function() {
+describe("VerifySignature", function() {
 
-//   it("Recover address from signature", async function() {
+  it("Recover address from signature", async function() {
 
-//     const network = await ethers.provider.getNetwork();
-//     let verifySignature;
+    const network = await ethers.provider.getNetwork();
+    let verifySignature;
 
-//     // Get the ContractFactory and Signers here.
-//     const accounts = await ethers.getSigners();
-//     const VerifySignature = await ethers.getContractFactory("VerifySignature");
-//     if (network.name == 'kovan')
-//       verifySignature = await VerifySignature.attach("0xB4615f9A9eAd41FB83195C734c0a3535462Ad3B4");
-//     else if (network.name == 'goerli')
-//       verifySignature = await VerifySignature.attach("0xB4615f9A9eAd41FB83195C734c0a3535462Ad3B4");
-//     else
-//       verifySignature = await VerifySignature.deploy();
+    // Get the ContractFactory and Signers here.
+    const accounts = await ethers.getSigners();
+    const VerifySignature = await ethers.getContractFactory("VerifySignature");
+    if (network.name == 'kovan')
+      verifySignature = await VerifySignature.attach("0xB4615f9A9eAd41FB83195C734c0a3535462Ad3B4");
+    else if (network.name == 'goerli')
+      verifySignature = await VerifySignature.attach("0xB4615f9A9eAd41FB83195C734c0a3535462Ad3B4");
+    else
+      verifySignature = await VerifySignature.deploy();
 
-//     let messageHash = ethers.utils.solidityKeccak256([ "address", "uint32", "bool" ], [ accounts[1].address, 0, false ]);
-//     const msgBytes = ethers.utils.arrayify(messageHash);
+    let messageHash = ethers.utils.solidityKeccak256([ "address", "uint32", "bool" ], [ accounts[1].address, 0, false ]);
+    const msgBytes = ethers.utils.arrayify(messageHash);
 
-//     //Sign the messageHash
-//     const signature = await accounts[0].signMessage(msgBytes);
-//     //Recover the address from signature
-//     const recoveredAddress = ethers.utils.verifyMessage(msgBytes, signature);
-//     //Expect the recovered address is equal to the address of accounts[0] 
-//     expect(recoveredAddress).to.equal(accounts[0].address);
+    //Sign the messageHash
+    const signature = await accounts[0].signMessage(msgBytes);
+    //Recover the address from signature
+    const recoveredAddress = ethers.utils.verifyMessage(msgBytes, signature);
+    //Expect the recovered address is equal to the address of accounts[0] 
+    expect(recoveredAddress).to.equal(accounts[0].address);
 
-//     const isValidSignature = await verifySignature.verifyC(accounts[0].address, accounts[1].address, 0, false, signature);
-//     expect(isValidSignature).to.be.true;
-//   });
-// });
+    const isValidSignature = await verifySignature.verifyMinter(accounts[0].address, accounts[1].address, 0, false, signature);
+    expect(isValidSignature).to.be.true;
+  });
+});
