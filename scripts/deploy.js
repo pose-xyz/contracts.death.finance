82b71c0a368038536c2f83f509f77382c44ce1f0
Eric Olszewski <eolszewski@gmail.com>
2022-02-17 10:29:24 -0600

    Add goerli tests, add bouts to constructor.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,10 +14,12 @@ async function main() {
   // console.log("ComposeArt deployed to:", composeArt.address);
 
   const FightClub = await ethers.getContractFactory("FightClub");
+
   const fightClub = await FightClub.deploy(
     "193660831688735064581587655956512620320321525841920",
     deployer.address,
     verifySignature.address,
+    5,
     "0xB3B3886F389F27BC1F2A41F0ADD45A84453F0D2A877FCD1225F13CD95953A86A"
   );
   console.log("FightClub deployed to:", fightClub.address);
